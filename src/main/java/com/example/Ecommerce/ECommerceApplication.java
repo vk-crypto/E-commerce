cb19db430c4f5dec1ffa0db838e3235bffa76869
vk-crypto <75367383+vk-crypto@users.noreply.github.com>
2020-12-24 14:40:42 +0530

    Update ECommerceApplication.java

--- src/main/java/com/example/Ecommerce/ECommerceApplication.java
+++ src/main/java/com/example/Ecommerce/ECommerceApplication.java
@@ -2,8 +2,10 @@ package com.example.Ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class ECommerceApplication {
 
 	public static void main(String[] args) {
